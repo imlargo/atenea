fb40c5e07bcfd90ed2949a5c5ef4d501817a8cd6
imlargo <jlargob@unal.edu.co>
2024-10-19 21:29:52 -0500

    eliminar cors

--- main.go
+++ main.go
@@ -8,30 +8,10 @@ import (
 )
 
 func contenidoHandler(w http.ResponseWriter, r *http.Request) {
-	allowedOrigins := []string{
-		"http://localhost:5173",
-		"https://pegaso.imlargo.dev",
-		"https://pegaso-git-develop-imlargos-projects.vercel.app",
-		"https://sia-extractor-contenidos.onrender.com",
-	}
-
-	origin := r.Header.Get("Origin")
-	allowed := false
-	for _, o := range allowedOrigins {
-		if o == origin {
-			allowed = true
-			break
-		}
-	}
-
-	if allowed {
-		w.Header().Set("Access-Control-Allow-Origin", origin)
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	} else {
-		http.Error(w, "CORS origin not allowed", http.StatusForbidden)
-		return
-	}
+	// Permitir cualquier origen
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	// Manejar solicitudes OPTIONS
 	if r.Method == http.MethodOptions {
