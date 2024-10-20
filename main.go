ee78707072abf5d7d38d26554db1ef63c2a71d5d
imlargo <jlargob@unal.edu.co>
2024-10-19 21:19:27 -0500

    chore: proteger rutas con cors

--- main.go
+++ main.go
@@ -8,11 +8,29 @@ import (
 )
 
 func contenidoHandler(w http.ResponseWriter, r *http.Request) {
+	allowedOrigins := []string{
+		"http://localhost:5173",
+		"https://pegaso.imlargo.dev",
+		"https://pegaso-git-develop-imlargos-projects.vercel.app",
+	}
 
-	// Agregar encabezados CORS
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	origin := r.Header.Get("Origin")
+	allowed := false
+	for _, o := range allowedOrigins {
+		if o == origin {
+			allowed = true
+			break
+		}
+	}
+
+	if allowed {
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	} else {
+		http.Error(w, "CORS origin not allowed", http.StatusForbidden)
+		return
+	}
 
 	// Manejar solicitudes OPTIONS
 	if r.Method == http.MethodOptions {
