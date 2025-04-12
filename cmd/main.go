b2a7d85cd610b57f349954354c5b942cb08a70cb
imlargo <jlargob@unal.edu.co>
2025-04-11 20:05:36 -0500

    feat: update Swagger documentation title and description for Atenea API

--- cmd/main.go
+++ cmd/main.go
@@ -32,8 +32,8 @@ func main() {
 
 	gin.SetMode(os.Getenv("GIN_MODE"))
 
-	docs.SwaggerInfo.Title = "Go API Master Example"
-	docs.SwaggerInfo.Description = "This a project is a example of API Rest in Go using Gin, Gorm & PostgreSQL."
+	docs.SwaggerInfo.Title = "Atenea API"
+	docs.SwaggerInfo.Description = "Your tool to find university course information quickly and easily."
 	docs.SwaggerInfo.Version = "1.0"
 	docs.SwaggerInfo.Host = "localhost:8080"
 	docs.SwaggerInfo.BasePath = "/v1"
